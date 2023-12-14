b52a904961acf1edcc8eb96f37a17b7cc292fef4
Anggit Utomo <pucukpisang97@gmail.com>
2023-12-14 19:38:38 +0700

    v1.6.2

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -1,7 +1,7 @@
 <div class="max-w-3xl flex justify-between items-center mx-auto px-4 py-8">
     <div class="flex">
         <h1 class="text-xl md:text-4xl text-teal-500">
-            <a href="{{ route('app.dashboard') }}">{{ env('APP_NAME') }}</a>
+            <a href="{{ route('app.dashboard') }}" wire:navigate.hover>{{ env('APP_NAME', 'anggitutomo.com') }}</a>
         </h1>
         <x-desktop-menu />
     </div>
